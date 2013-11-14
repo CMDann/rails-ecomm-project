f36ba24e3a05c4941a725d8e8afb13209e5cb5b8
Dann Blair <cmdann@gmail.com>
2013-11-14 13:36:05 -0600

    Added reviews to the active admin section, changed pounds symbol to dollars.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
     column :category
     column :price, :sortable => :price do |product|
       div :class => "price" do
-        number_to_currency product.price, :unit => "&pound;"
+        number_to_currency product.price, :unit => "&dollar;"
       end
     end
     default_actions
