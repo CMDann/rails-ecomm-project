4d1777e1a770605c858fb2e1871524a5ee90081c
Daniel Blair <CMDann.Development@gmail.com>
2013-11-24 12:27:50 -0600

    Added pagination to index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index # 
-    @products   = Product.order(:name).page(params[:page])
+    @products   = Product.order(:name).page(params[:page]).per(3)
   end # Load the app/views/home/index
 
   def show
